653aa914f55e54e17abbfec821c3659d18c5cb9a
Guilherme Martins Volpe <guilherme.volpe@useargo.com>
2023-09-02 13:43:17 -0300

    Ajuste Botões

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,7 +9,7 @@ export class ProductsComponent {
   products = [
     { id: 1, name: 'Therra', description: 'Chopp mais leve sem glutém', price: 30, img: 'assets/imgs/therra-rotulo.png' },
     { id: 2, name: 'Osiris', description: 'Nossa Ipa', price: 35, img: 'assets/imgs/osiris-rotulo.png' },
-    { id: 2, name: 'Black', description: 'Cervejica Gostosa', price: 36, img: 'assets/imgs/osiris-rotulo.png'  }
+    { id: 3, name: 'Black', description: 'Cervejica Gostosa', price: 45, img: 'assets/imgs/osiris-rotulo.png'  }
     // ... add more products as needed
   ];
 
